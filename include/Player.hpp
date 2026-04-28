231c27a9534a3fa3c9c577593418ee2574a6d30c
asma-bensliman <ab.asma.bensliman@gmail.com>
2026-04-28 12:56:48 +0200

    Revert "Merge branch 'main' of https://github.com/haninitz/2PROJ"
    This reverts commit 8f4d511a91e1f25e4f6137454b3e0a38697233a2, reversing
    changes made to 8a1cbf80e7b33e12e7abb962228fc296d84822cc.

--- include/Player.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-// Représente un joueur humain
-struct Player {
-    std::string name;   // Nom du joueur
-    int gold;           // Or disponible pour produire des unités
-};
